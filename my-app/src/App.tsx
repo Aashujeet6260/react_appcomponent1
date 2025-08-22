f920fb70ff814fdf3f90d2839cc51dfb029ed9d3
Aashujeet6260 <aashujeet2001@gmail.com>
2025-08-22 22:11:45 +0530

    update_app.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,10 +8,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
